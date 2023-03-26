d9310aa756b3dad72b38dd58761840269559d124
Fabio Mayorga <fabiomayorgad@hotmail.com>
2023-03-26 18:14:11 -0500

    aded test for Counter Component

---
+++ src/Components/Counter/Counter.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { screen, render, fireEvent } from '@testing-library/react';
+import { Counter } from './Counter';
+
+describe('Test Counter Component', () => {
+    test('renders initial value provided in props', () => {
+        const initialValue = 0;
+        render(<Counter initialValue={initialValue} />);
+        const counterLabel = screen.getByText(`${initialValue}`);
+        expect(counterLabel).toBeInTheDocument();
+    })
+
+    test('increments the displayed value when "increment" button is clicked', () => {
+        const initialValue = 5;
+        render(<Counter initialValue={initialValue} />);
+        const counterLabel = screen.getByText(`${initialValue}`);
+        const incrementButton = screen.getByText(/Increase Counter/i);
+
+        fireEvent.click(incrementButton);
+
+        expect(counterLabel).toHaveTextContent(`${initialValue + 1}`);
+    });
+
+    test('decrements the displayed value when "decrement" button is clicked', () => {
+        const initialValue = 5;
+        render(<Counter initialValue={initialValue} />);
+        const counterLabel = screen.getByText(`${initialValue}`);
+        const decrementButton = screen.getByText(/Decrease Counter/i);
+
+        fireEvent.click(decrementButton);
+
+        expect(counterLabel).toHaveTextContent(`${initialValue - 1}`);
+    });
+});
