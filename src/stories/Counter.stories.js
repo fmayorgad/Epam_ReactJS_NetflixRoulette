e627bb60542f56df97599fd82e70dd1f19a962b4
Fabio Mayorga <fabiomayorgad@hotmail.com>
2023-04-04 18:52:08 -0500

    added story for Counter component

---
+++ src/stories/Counter.stories.js
@@ -0,0 +1,10 @@
+import { Counter } from '../Components/Counter/Counter';
+
+export default {
+    title: 'Just a Counter Component',
+    component: Counter,
+}
+
+const Template = () => <Counter/>;
+
+export const Default = Template.bind({});
