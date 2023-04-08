22c1ab85704abbc07f3d36996d1b9d2231cd84cb
Fabio Mayorga <fabiomayorgad@hotmail.com>
2023-04-07 21:48:45 -0500

    added component styles an elements

--- src/Components/MovieDetails/MovieDetails.js
+++ src/Components/MovieDetails/MovieDetails.js
@@ -4,23 +4,31 @@ import './MovieDetails.css';
 export class MovieDetails extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {...props};
+        this.state = { ...props };
     }
 
     render() {
         return (
             <section className='MovieDetailsContainer'>
-                <img alt="moviePoster" className='movieTileImage' src={this.state.urlImage} />
+                <img alt="moviePoster" className='movieDetailImage' src={this.state.urlImage} />
                 <div className='detailsDescriptionContainer'>
-                    <h1 className='movieDetailTitle'>{this.state.name}</h1>
-                    <div className='movieRatingLabel'>
-                        {this.state.rating}
+                    <div className='movieDetailMainInfoContainer'>
+                        <h1 className='movieDetailTitle'>{this.state.name}</h1>
+                        <span className='movieRatingLabel'>
+                            {this.state.rating}
+                        </span>
                     </div>
+
+                    <p className='movieDetailsGenres'>
+                        {
+                            this.state.genres.length > 0 ?
+                                (this.state.genres.length > 2 ? this.state.genres.join(' , ') : this.state.genres.join(' & ')) : '-'
+                        }</p>
                     <div className='movieDetailContainer'>
                         <span>{this.state.releaseYear}</span>
                         <span>{this.state.duration}</span>
                     </div>
-                    <p>{this.state.description}</p>
+                    <p className='movieDetailDescription'>{this.state.description}</p>
                 </div>
             </section>
         );
