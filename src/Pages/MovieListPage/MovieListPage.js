5ba53cec71d2ba7f448d1b425760cc93d7fb31c3
Fabio Mayorga <fabiomayorgad@hotmail.com>
2023-04-26 16:31:31 -0500

    added logic to filters and searchs

--- src/Pages/MovieListPage/MovieListPage.js
+++ src/Pages/MovieListPage/MovieListPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import { SearchForm } from '../../Components/SearchForm/SearchForm';
 import { GenreSelector } from '../../Components/GenreSelector/GenreSelector';
 import { SortControl } from '../../Components/SortControl/SortControl';
@@ -10,7 +10,7 @@ import './MovieListPage.css';
 const genreList = [
     { id: 0, name: 'all' },
     { id: 1, name: 'drama' },
-    { id: 2, name: 'terror' },
+    { id: 2, name: 'horror' },
     { id: 3, name: 'comedy' },
     { id: 4, name: 'documentary' },
     { id: 5, name: 'crime' },
@@ -27,27 +27,6 @@ const sortOptions = [
     }
 ];
 
-const movieProps = [{
-    urlImage: 'https://lumiere-a.akamaihd.net/v1/images/image_b88fdde2.jpeg?region=0,0,540,810&width=480',
-    name: 'Titanic in the desert',
-    releaseYear: 2033,
-    genres: ['Drama', 'Action', 'Romance'],
-    rating: 9.4,
-    duration: '2h 40min',
-    description: 'this is a good movie',
-    id: 1
-},
-{
-    urlImage: 'https://cdn.marvel.com/content/1x/antmanandthewaspquantumania_lob_crd_03.jpg',
-    name: 'ANT MAN QUANTUMANIA',
-    releaseYear: 2023,
-    genres: ['Drama', 'Action'],
-    rating: 6.4,
-    duration: '1h 50min',
-    description: 'Solo se sabe que Scott Lang será un personaje clave para este fin. Vemos en los tráilers que Kang negocia con Ant-Man para que ambos salgan beneficiados, pero al final el villano le traiciona.    Lang desea recuperar el tiempo perdido junto a su hija, mientras que el villano desea salir del mundo cuántico. Por este motivo, Kang envía al protagonista a conseguir un raro elemento en el microverso.',
-    id: 2
-}
-];
 
 export function MovieListPage(props) {
 
@@ -55,16 +34,28 @@ export function MovieListPage(props) {
     const [selectedGenre, setSelectedGenre] = useState(0);
     const [sortValue, setSortValue] = useState(1);
     const [selectedMovieTile, setselectedMovieTile] = useState(null);
-    const [movieList, setMovieList] = useState(movieProps);
-    let consumingFetch = false;
-    let controller = new AbortController();
+    const [movieList, setMovieList] = useState([]);
+    const [consumingFetch, setConsumingFetch] = useState(false);
+    const [controller] = useState(new AbortController());
+    const [queryParams] = useState(new URLSearchParams());
+    const defaultSearch = 'Jumanji';
 
     const onSearch = function (input) {
+        queryParams.set('searchBy', 'title');
+        queryParams.set('search', input);
+        console.log(input)
         setSearchValue(input);
     }
 
     const onSelectGenre = function (genre) {
         setSelectedGenre(genre);
+        if (genre.id === 0) {
+            queryParams.delete('searchBy');
+            queryParams.delete('search');
+        } else {
+            queryParams.set('searchBy', 'genres');
+            queryParams.set('search', genre.name);
+        }
     }
 
     const onSort = function (sortValue) {
@@ -72,34 +63,50 @@ export function MovieListPage(props) {
     }
 
     const onSelectMovieTile = function (movie) {
-        console.log(movie)
         setselectedMovieTile(movie);
     }
 
-    const searchMovies = async function () {
+    const searchMovies = useCallback(async () => {
 
         consumingFetch && controller.abort();
 
-        consumingFetch = true;
+        setConsumingFetch(true);
 
-        controller = new AbortController();
         const signal = controller.signal
 
-        // Register a listenr.
-        signal.addEventListener("abort", () => {
-            console.log("aborted!")
-        })
-        const response = await fetch("http://localhost:4000/movies?searchBy=title", {
+        //create queryparams
+        //searchValue.length > 0 && queryParams.append('title', searchValue);
+
+        const response = await fetch("http://localhost:4000/movies?" + queryParams.toString(), {
             signal: controller.signal
         });
+
         const movies = await response.json();
 
-        console.log(movies)
-    }
+        let moviesmapped = movies.data.map(m => {
+            let movie = {};
+            movie.urlImage = m.poster_path;
+            movie.name = m.title;
+            movie.releaseYear = m.release_date;
+            movie.genres = m.genres;
+            movie.rating = m.vote_average;
+            movie.duration = m.runtime;
+            movie.description = m.overview;
+            movie.id = m.id;
+            return movie;
+        });
+
+        signal.addEventListener("abort", () => {
+            console.log("aborted!")
+        });
+
+        setConsumingFetch(false);
+        setMovieList(moviesmapped)
+    });
 
     useEffect(() => {
         searchMovies();
-    }, []);
+    }, [searchValue, selectedGenre]);
 
     return (
         <>
@@ -108,7 +115,7 @@ export function MovieListPage(props) {
                 <section>
                     {
                         selectedMovieTile ? <MovieDetails {...selectedMovieTile} />
-                            : <SearchForm onSearch={onSearch} searchQuery='Drama' />
+                            : <SearchForm onSearch={onSearch} searchQuery={defaultSearch} />
                     }
                 </section>
 
